c16efebfce8800aa04f8dc446ebd39ffcc235cbb
Vanessa_Margareth <vsmargareth@hotmail.com>
2023-03-03 12:24:47 +0000

    modo escuro concluido, falta estilixar botão

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
-let buttonRef = document.querySelector('button');
-window.addEventListener('click', blabckMode)
+/*
+-Seleção de componentes no HTML tem sempre ref no final
+-Modo escuro = blackMode
+*/
 
+//seleção de componentes no HTML
+let buttonRef = document.querySelector('button');
 let bodyRef = document.querySelector('body');
 let h1Ref = document.querySelector('h1');
+let itemRef = document.querySelectorAll('.item')
+let pRef = document.querySelectorAll('p');
+let h2Ref = document.querySelectorAll('h2')
+
+mudaTextoBotao()
+
+//Monitoramento de cliques
+window.addEventListener('click', blabckMode)
+//mudar texto do botão
+function mudaTextoBotao() {
+  if (buttonRef.textContent === "Modo escuro") {
+    buttonRef.textContent = "Modo claro";
+  } else {
+    buttonRef.textContent = "Modo escuro";
+  }
+}
+
+//Mudando todos os itens 
+function mudaItens() {
+  itemRef.forEach(function (selecionaItens) {
+    selecionaItens.classList.toggle("itemDarkMode")
+  })
+}
+
+//Mudando todos os paragráfos
+function mudaParagrafos() {
+  pRef.forEach(function (mudaParagrafo) {
+    mudaParagrafo.classList.toggle("textDarkMode")
+  })
+}
+//Mudando todos os H's 2
+function mudaTodosH2() {
+  h2Ref.forEach(function (mudaHs2) {
+    mudaHs2.classList.toggle("textDarkMode")
+  })
+}
+
+//Fução para ativar modo escuro
 function blabckMode() {
+  mudaTextoBotao()
   bodyRef.classList.toggle('bgDarkMode');
   h1Ref.classList.toggle('titleDarkMode');
-  console.log("deu certo no js")
+  mudaItens();
+  mudaParagrafos();
+  mudaTodosH2();
 }
